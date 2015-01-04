0b80dbfcf25f6e842cdacd3ed510ce5cdb4b3e75
Mike Kreiser <mkreise2@illinois.edu>
2015-01-03 20:21:11 -0500

    calc

--- supersecretdev/index.js
+++ supersecretdev/index.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
     	loopHorizontal: false,
     	sectionsColor: ['#00A8C6', '#FFF', '#000', '#FFF'],
     	anchors: ['frontpage','intro','dev','4'],
-    	verticalCentered: true 	
+    	verticalCentered: true,
+    	slidesNavigation: true
     });
 
     $.fn.fullpage.reBuild();
